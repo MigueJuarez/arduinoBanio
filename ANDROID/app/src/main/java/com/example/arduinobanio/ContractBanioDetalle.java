df2e59bbd4740a2692f0b2d6da6130fa2cf57721
miguel.juarez <miguel.juarez@despegar.com>
2022-11-10 14:06:00 -0300

    arreglo permisos de android

--- ANDROID/app/src/main/java/com/example/arduinobanio/ContractBanioDetalle.java
+++ ANDROID/app/src/main/java/com/example/arduinobanio/ContractBanioDetalle.java
@@ -1,5 +1,6 @@
 package com.example.arduinobanio;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.os.Handler;
 
@@ -7,32 +8,26 @@ public interface ContractBanioDetalle {
 
     interface View {
         void actualizarEstado(String estado);
-
         void showMsg(String msg);
-
         void finishView();
     }
 
-    interface Model {
-
-        void establecerConexionDevice(ContractBanioDetalle.Model.CallBackToView cb, BluetoothDevice device);
-
-        Handler Handler_Msg_Hilo_Principal (CallBackToView cb);
-
-        interface CallBackToView {
-            void actualizarEstado(String estado);
-
-            void showMsg(String msg);
-
-            void finishView();
-        }
-
-    }
-
     interface Presenter {
+        void pedirPermisos(Activity activity);
         void establecerConexionDevice(BluetoothDevice device);
-
+        void showMsg(String msg);
         Handler Handler_Msg_Hilo_Principal();
     }
 
+    interface Model {
+        void pedirPermisos(Activity activity);
+        void establecerConexionDevice(ContractBanioDetalle.Model.CallBackToView cb, BluetoothDevice device);
+
+        Handler Handler_Msg_Hilo_Principal (CallBackToView cb);
+        interface CallBackToView {
+            void actualizarEstado(String estado);
+            void showMsg(String msg);
+            void finishView();
+        }
+    }
 }
